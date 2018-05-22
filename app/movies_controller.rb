5ea5c7ad02c76e66fde38405a7b36ba5e939fac6
Holden Altaffer <haltaffer@gmail.com>
2018-05-22 21:41:48 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -23,9 +23,9 @@ def can_be_created_with_a_hash_of_attributes
 end
 
 def can_be_created_in_a_block(args = {})
-  # Movie.create do |m|
-  #   __
-  # end
+  Movie.create do |m|
+    m.args.keys = args.values
+  end
 end
 
 def can_get_the_first_item_in_the_database
